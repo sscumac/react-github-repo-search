33ba0e060e4dc75613e42eb379e681352b2302b7
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-05-01 19:14:22 +0200

    added form - input not yet working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,27 +3,38 @@ import Button from './Button'
 import InputField from './InputField'
 
 function Header(props) {
+
+  function handleSubmit() {
+    console.log(props.submitInput);
+    (props.submitInput === "") ? alert("Empty search - Please type something to search.") : props.setSearchInput(props.submitInput);
+  }
+
   return(
     <div id={styles.header}>
       <h1>Github Repositories</h1>
       <div className={styles.searchBar}>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            <InputField
+              type={'text'} 
+              placeholder={props.placeholder}
+              onChange={(eve) => {
+                  (eve.target.value.length > 4) && props.setSearchInput(eve.target.value)
+                  props.setSubmitInput(eve.target.value)
+                  props.setPlaceholder('')
+                }
+              }
+            />
+          </label>
+          <input type="submit" value="Search" />
+        </form>
         
-        <InputField
-          type={'text'} 
-          placeholder={props.placeholder}
-          onChange={(eve) => {
-              (eve.target.value.length > 4) && props.setSearchInput(eve.target.value)
-              props.setSubmitInput(eve.target.value)
-              props.setPlaceholder('')
-            }
-          }
-        />
-        
-        <Button text={'Search'} onClick={() => {
+        {/* <Button text={'Search'} onClick={() => {
               (props.submitInput === "") ? alert("Empty search - Please type something to search.") : props.setSearchInput(props.submitInput);
             }
           }
-        />
+        /> */}
       </div>
     </div>
   )
