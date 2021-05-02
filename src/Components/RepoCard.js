36ac6378d8821f95c97107d7cfb9b00c4c4a8ef5
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-05-02 17:36:22 +0200

    card div replace by a tag

--- src/Components/RepoCard.js
+++ src/Components/RepoCard.js
@@ -4,42 +4,44 @@ import Button from './Button';
 function RepoCard(props) {
 
   return(
-    <div className={styles.card} onClick={() => window.open(props.repo.html_url, "_blank")}>
-    <div className={styles.name}>{props.repo.name}</div>
-    <div className={styles.desc}>{props.repo.description}</div>
-    <div className={styles.footer}>
+    <a className={styles.card} target="_blank" href={props.repo.html_url}>
+      <div className={styles.name}>{props.repo.name}</div>
+      <div className={styles.desc}>{props.repo.description}</div>
+      <div className={styles.footer}>
 
-      {/* Button Search Result List */}
-      {(props.listType !== "Favorites") &&
-        <Button 
-          onClick={(eve) =>  {
-              eve.stopPropagation();
-              props.onFavorite(props.repo);
+        {/* Button Search Result List */}
+        {(props.listType !== "Favorites") &&
+          <Button 
+            onClick={(eve) =>  {
+              console.log(eve.target)
+                eve.preventDefault()
+                // eve.stopPropagation();
+                props.onFavorite(props.repo);
+              }
             }
-          }
-          text={(props.repo.favorite) ? 'unsave' : 'save'}
-          color={(props.repo.favorite) && 'white'}
-          bgcolor={(props.repo.favorite) && '#017F80'}
-          className={styles.btn}
-        />
-      }
+            text={(props.repo.favorite) ? 'unsave' : 'save'}
+            color={(props.repo.favorite) && 'white'}
+            bgcolor={(props.repo.favorite) && '#017F80'}
+            className={styles.btn}
+          />
+        }
 
-      {/* Button Favorite List */}
-      {(props.listType === "Favorites") &&
-        <Button
-          onClick={(eve) => {
-              eve.stopPropagation();
-              props.onFavorite(props.repo);
+        {/* Button Favorite List */}
+        {(props.listType === "Favorites") &&
+          <Button
+            onClick={(eve) => {
+                eve.stopPropagation();
+                props.onFavorite(props.repo);
+              }
             }
-          }
-          text={'Unsave'}
-          color={'white'}
-          bgcolor={'#017F80'}
-          className={styles.btn}
-        />
-      }
-      </div>
-    </div>
+            text={'Unsave'}
+            color={'white'}
+            bgcolor={'#017F80'}
+            className={styles.btn}
+          />
+        }
+        </div>
+    </a>
   )
 }
 
